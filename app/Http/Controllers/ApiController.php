93324e2bb038d4b5b1f6545fc1a866a0ed6ff2d7
Rafael Gomes Kohls <rafael288.gk@gmail.com>
2024-07-04 10:33:58 -0300

    Ajustes na identacao

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,31 +9,20 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-   public function show($id){
-
-    $user = User::find($id);
-
-
-    if (!$user) {
-        return ApiResponse::format('404', 'User not found', null)->setStatusCode(404);
-    } else{
-
-    return ApiResponse::format('200', 'User found', $user);
+    public function show($id)
+    {
+        $user = User::find($id);
+        if (!$user) {
+            return ApiResponse::format('404', 'User not found', null)->setStatusCode(404);
+        } else {
+            return ApiResponse::format('200', 'User found', $user);
+        }
     }
 
-
-   }
-
-
-
-   public function store(StoreUserRequest $request)
-{
-    
-    $data = $request->validated();
-
-    
-    $user = User::create($data);
-
-    return ApiResponse::format('success', 'User created successfully', $user);
-}
+    public function store(StoreUserRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::create($data);
+        return ApiResponse::format('200', 'Success', $user);
+    }
 }
